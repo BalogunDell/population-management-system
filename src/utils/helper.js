265013df1dbc8dd8447e21581c07ff605ca8682d
Balogun Abiodun Delight <delighteddell@gmail.com>
2019-04-01 06:04:59 +0100

    Population Management System

---
+++ src/utils/helper.js
@@ -0,0 +1,18 @@
+import statusCodes from './statusCodes';
+import customResponseObject from './responses';
+import { duplicateError } from './messages';
+
+const mongoDuplicateKeyCode = 11000;
+
+export const getTotalResidents = (maleResidents, femaleResidents) => {
+  return maleResidents + femaleResidents;
+};
+
+export const errorHandler = (res, error) => {
+  console.log(error, 'error');
+  if (error && error.code === mongoDuplicateKeyCode) {
+    return customResponseObject(res, duplicateError, statusCodes.duplicate);
+  }
+  const errors = error.map(err => err.msg);
+  return customResponseObject(res, errors, statusCodes.badRequest);
+}
